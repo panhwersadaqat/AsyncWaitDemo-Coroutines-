5a0557e804bfb01434fbdce6c6579b28b68f130b
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2021-12-15 11:07:48 +0500

    initial commit

---
+++ app/src/main/java/com/example/asyncwaitdemo/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.asyncwaitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CoroutineScope(IO).launch {
+            Log.i("MyTag", "Calculation Started...")
+            val stock1 = async {
+                getStock1()
+            }
+            val stock2 = async {
+                getStock2()
+            }
+            val result = stock1.await()+stock2.await()
+
+            Log.i("MyTag", "Calculation result $result")
+        }
+    }
+
+    private suspend fun getStock1() : Int {
+        delay(10000)
+        Log.i("MyTag","stock 1 returned")
+        return 35000
+    }
+
+    private suspend fun getStock2() : Int {
+        delay(8000)
+        Log.i("MyTag","stock 2 returned")
+        return 5000
+    }
+}
